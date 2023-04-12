bc3b9f1ed2d9babd0416998916facc3776e9e4e9
nellie236 <nicolebraccili@gmail.com>
2023-04-12 15:19:31 -0400

    044 - New Ingredients, Potions, Recipe Pages

--- Assets/Scripts/ToolTips.cs
+++ Assets/Scripts/ToolTips.cs
@@ -6,11 +6,13 @@ public class ToolTips : MonoBehaviour
 {
     public bool toolTips;
     public TextMesh currentKey;
+    public GameObject me;
     // Start is called before the first frame update
     void Start()
     {
         toolTips = true;
-        this.gameObject.SetActive(false);
+        me = this.gameObject;
+        me.SetActive(false);
     }
 
     // Update is called once per frame
@@ -19,14 +21,14 @@ public class ToolTips : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (toolTips)
+        if (toolTips && collision.CompareTag("ToolTips"))
         {
             //currentKey = collision.GetComponent<Key>
-            gameObject.SetActive(true);
-            
-
+            me.SetActive(true);
         }
     }
+
+
 }
